af2f4e6e3ee825a76764f55451bd0b913f2cdf7a
Aryangupta <aryan072806@gmail.com>
2026-01-21 01:57:27 +0530

    added state.py

--- agents/state.py
+++ agents/state.py
@@ -23,6 +23,7 @@ class AgentAnalysis(BaseModel):
     confidence: float = Field(description="Confidence score 0.0 to 1.0")
     severity: SeverityLevel
     reasoning: str
+    title:str
     
 class Location(BaseModel):
     lat: float
@@ -34,28 +35,20 @@ class AgentState(TypedDict):
     location: Location
     address: str
     email: str
-    interests: List[str]
     description:str
     reportId:str
     geohash:str
     locality_imageUrl=Optional[str]
     locality_email=Optional[str]
     locality_userId=Optional[str]
-
-
     water_analysis: Optional[AgentAnalysis]
     waste_analysis: Optional[AgentAnalysis]
     infra_analysis: Optional[AgentAnalysis]
     electric_analysis: Optional[AgentAnalysis]
-
+    uncertain_analysis:Optional[AgentAnalysis]
     aiAnalysis: Optional[str]
     severity: Optional[SeverityLevel]
     assigned_category: Optional[ReportCategory]
     status: ReportStatus
     route:str
     tool:Literal["SAVE","UPDATE"]
-    
-    upvotes: int
-    downvotes: int
-    createdAt: str
-    updatedAt: str
